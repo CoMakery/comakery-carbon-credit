3cdc810a9a42f42ec7f2e17a5c7cb7683127d4e0
Noah Thorp <noah@comakery.com>
2019-11-24 23:15:18 -0800

    mint carbon credits from a certificate

--- contracts/CarbonCredit.sol
+++ contracts/CarbonCredit.sol
@@ -5,9 +5,20 @@ import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 // ERC20Detailed("CoMakery Carbon Credits", "CCC", 18)
 contract CarbonCredit is ERC20, Ownable, ERC20Detailed {
+
+  event DepositCarbonCreditsFromCertificate(
+    address indexed approvedBy,
+    uint256 indexed value,
+    string ifpsHashOfCarbonCreditCertificate);
+
   constructor(string memory _name, string memory _symbol)
   ERC20Detailed(_name, _symbol, 18)
   public {
     
+  }
+
+  function depositCarbonCreditsFromCertificate(uint256 _value, string memory _ipfsHashOfCarbonCreditCertificate) public {
+    _mint(owner(), _value);
+    emit DepositCarbonCreditsFromCertificate(owner(), _value, _ipfsHashOfCarbonCreditCertificate);
   }
 }
--- test/carbon_credit.js
+++ test/carbon_credit.js
@@ -1,8 +1,10 @@
+const truffleAssert = require('truffle-assertions')
 const CarbonCredit = artifacts.require("CarbonCredit")
 
 contract('CarbonCredit', function(accounts) {
   var owner
   var token
+  var emptyAddress = web3.utils.padLeft(0x0, 40)
   
   beforeEach(async () => {
     owner = accounts[0]
@@ -18,4 +20,25 @@ contract('CarbonCredit', function(accounts) {
     assert.equal(await token.decimals(), 18)
   })
 
+  it('can depositCarbonCreditsFromCertificate', async () => {
+    let ipfsCarbonCertificateHash = "QmcFULbtwMYLwe2cUdUvvWNQVcRsqKCVgFbaUGgseJcpsa"
+    let tx = await token.depositCarbonCreditsFromCertificate(100, ipfsCarbonCertificateHash)
+
+    truffleAssert.eventEmitted(tx, 'Transfer', (ev) => {
+      assert.equal(ev.from, emptyAddress)
+      assert.equal(ev.to, owner)
+      assert.equal(ev.value, 100)
+      return true
+    })
+    
+    truffleAssert.eventEmitted(tx, 'DepositCarbonCreditsFromCertificate', (ev) => {
+      assert.equal(ev.ifpsHashOfCarbonCreditCertificate, ipfsCarbonCertificateHash)
+      assert.equal(ev.value, 100)
+      assert.equal(ev.approvedBy, owner)
+      return true
+    })
+
+    assert.equal(await token.totalSupply(),100)
+    assert.equal(await token.balanceOf(owner), 100)
+  })
 })
